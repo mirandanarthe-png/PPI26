50b0aadc8094b4e7ec2b00451a10d57c09478cad
mirandanarthe-png <miranda.narthe@escolar.ifrn.edu.br>
2026-05-06 19:19:19 +0000

    Aula do dia 6/5  itroduçao a javascript

---
+++ PPI26.js
@@ -0,0 +1,28 @@
+// aula 6/5
+console.log("Bem vindo a PPI!");
+// variaveis
+let name = "joão";
+let age = 17;
+let isStudent = true;
+
+const PI = 2.14;
+// PI = 4;
+console.log(PI);
+
+// operadores
+let x = 5 + 5;
+let y = "5" + 5;
+let z = "Hello " + 5;
+
+console.log(x, y, z);
+console.log(typeof x);
+
+// operadores relacionais
+console.log('5' === 5);
+
+// operadores de incrementos
+x = x++;
+console.log(x++);
+console.log(x);
+console.log(++x);
+
